2f5529b62101009c6be07af7ecb3acd62ab0474c
htay-htay-thwe <htayhtaythwe962@gmail.com>
2024-04-11 21:58:01 +0700

    good

---
+++ DotNetTrainingBatch4.ConsoleApp/AdoDotNetExample.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetTrainingBatch4.ConsoleApp
+{
+    internal class AdoDotNetExample
+    {
+        private readonly SqlConnectionStringBuilder _sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+         DataSource = "DESKTOP-QNI7OO1",
+         InitialCatalog =  "DotNEtTrainingBatch4",
+         UserID = "sa",
+         Password = "sasa@123",
+        };
+
+        public void Read()
+        {
+            SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+ 
+            SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
+            connection.Open();
+            string query = "select * from table_1";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sqlDataAdapter.Fill(dt);
+
+            connection.Close();
+            foreach (DataRow dr in dt.Rows)
+            {
+                Console.WriteLine("Blog Id =>" + dr["BlodId"]);
+                Console.WriteLine("Blog Title =>" + dr["BlogTitle"]);
+                Console.WriteLine("Blog Author =>" + dr["BlogAuthor"]);
+                Console.WriteLine("Blog Content =>" + dr["BlogContent"]);
+                Console.WriteLine("--------------------------");
+
+            }
+        }
+        public void Create(string title, string author, string content)
+        {
+            SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+
+            string query = @"INSERT INTO [dbo].[Table_1]
+       ([BlogTitle]
+           , [BlogAuthor]
+           , [BlogContent])
+     VALUES
+           (@BlogTitle
+           ,@BlogAuthor
+           ,@BlogContent)";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@BlogTitle", title);
+            cmd.Parameters.AddWithValue("@BlogAuthor", author);
+            cmd.Parameters.AddWithValue("@BlogContent", content);
+            int result = cmd.ExecuteNonQuery();
+            connection.Close();
+            string message = result > 0 ? "Saving Successful." : "Saving Failed.";
+            Console.Write(message);
+        }
+    }
+}
--- DotNetTrainingBatch4.ConsoleApp/Program.cs
+++ DotNetTrainingBatch4.ConsoleApp/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+/*using System.Data;
+using System.Data.SqlClient;
+
 Console.WriteLine("Hello, World!");
+
+SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+stringBuilder.DataSource = "DESKTOP-QNI7OO1";
+stringBuilder.InitialCatalog = "DotNEtTrainingBatch4";
+stringBuilder.UserID = "sa";
+stringBuilder.Password = "sasa@123";
+SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
+connection.Open();
+string query = "select * from table_1";
+SqlCommand cmd =new SqlCommand(query, connection);
+SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+DataTable dt =new DataTable();
+sqlDataAdapter.Fill(dt);
+
+connection.Close();
+foreach(DataRow dr in dt.Rows)
+{
+    Console.WriteLine("Blog Id =>" + dr["BlodId"]);
+    Console.WriteLine("Blog Title =>" + dr["BlogTitle"]);
+    Console.WriteLine("Blog Author =>" + dr["BlogAuthor"]);
+    Console.WriteLine("Blog Content =>" + dr["BlogContent"]);
+    Console.WriteLine("--------------------------");
+
+}
+Console.ReadKey();
+*/
+using DotNetTrainingBatch4.ConsoleApp;
+
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+//adoDotNetExample.Read();//
+adoDotNetExample.Create("title", "author", "content");
+Console.ReadLine();
+
